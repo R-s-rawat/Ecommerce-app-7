1a81df05890fc7a2479578db223ab02721a4da1b
R-s-rawat <rohitrawat590590@gmail.com>
2025-05-22 06:42:22 +0530

    importd 1st-route i.e authRoute in server.js(main)

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 // const colors = require('colors');
 
 // // below is module way (for import export (its newer way es6(ecmaSCRIPT 2015- i.e._es-v.6)))
-import express from 'express';
-import colors from 'colors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import connectDB from './config/db.js'; // // in type:module (es6) for import-export(within app), file extensions are compulsory
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js"; // // in type:module (es6) for import-export(within app), file extensions are compulsory
+import authRoutes from './routes/authRoute.js';
 
 // configure env - (as our env file is in root, so no location defining inside config method(i.e. not defining any object in config() as .env is already in root path))
-dotenv.config()
+dotenv.config();
 
 // database config
 connectDB();
@@ -19,19 +20,25 @@ connectDB();
 const app = express();
 
 // middlewares
-app.use(express.json()) // // in request & response, json data transfer (by default feature in express)
-app.use(morgan('dev')) // // for knowing which api getting called/requested (just for local testing) - *Not for prod.
+app.use(express.json()); // // in request & response, json data transfer (by default feature in express)
+app.use(morgan("dev")); // // for knowing which api getting called/requested (just for local testing) - *Not for prod.
+
+// routes
+app.use('/api/v1/auth', authRoutes);
 
 // rest api
-app.get('/', (req,res)=>{
-    res.send( '<h1>welcome to ecommerce app</h1>');
-})
+app.get("/", (req, res) => {
+  res.send("<h1>welcome to ecommerce app</h1>");
+});
 
 // port
 // nodeJS uses process.env
 const PORT = process.env.PORT || 8080;
 
-// run listen 
-app.listen(PORT,()=>{
-    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-})
+// run listen
+app.listen(PORT, () => {
+  console.log(
+    `server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
+});
