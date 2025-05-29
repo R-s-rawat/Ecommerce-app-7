7712537307d32cd39c94c88cfa5b24f8947a513e
R-s-rawat <rohitrawat590590@gmail.com>
2025-05-29 06:22:37 +0530

    besides NavLink - use LINK for normal ones........

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,44 +1,73 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-   <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <navlink to="/" className="navbar-brand" href="#">
-        Hidden brand
-      </navlink>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <navlink to="/" className="nav-link active" aria-current="page" href="#">
-            Home
-          </navlink>
-        </li>
-        <li className="nav-item">
-          <navlink to="/register" className="nav-link" href="#">
-            Register
-          </navlink>
-        </li>
-        <li className="nav-item">
-          <navlink to="/login" className="nav-link" href="#">
-            Login
-          </navlink>
-        </li>
-        <li className="nav-item">
-          <navlink to="/cart" className="nav-link" href="#">
-            Cart (0)
-          </navlink>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          {/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link to="/" className="navbar-brand">
+             Ecommerce App
+            </Link>
+            {/* // BS-5 utility classes  (ME)margin-end: auto && (MS)margin-start: auto*/}
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              {/* // Home */}
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+              {/* // Home Ends */}
+              {/*------------------------------------------------------- */}
+              {/* // Category */}
+              <li className="nav-item">
+                <NavLink to="/category" className="nav-link">
+                  Category
+                </NavLink>
+              </li>
+              {/* //Category Ends */}
+                 {/*------------------------------------------------------- */}
+              {/* //Register */}
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Register
+                </NavLink>
+              </li>
+              {/* //Register Ends */}
+                 {/*------------------------------------------------------- */}
+              {/* //Login */}
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              </li>
+              {/* // Login Ends */}
+                 {/*------------------------------------------------------- */}
+              {/* // Cart */}
+              <li className="nav-item">
+                <NavLink to="/cart" className="nav-link">
+                  Cart (0)
+                </NavLink>
+              </li>
+              {/* // Cart Ends */}
+            </ul>
+          </div>
+          {/* /////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+        </div>
+      </nav>
     </>
   );
 };
