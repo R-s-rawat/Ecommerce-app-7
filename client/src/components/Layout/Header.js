9594cecafe7a8f7e4cb873753fa460cd37fda561
R-s-rawat <rohitrawat590590@gmail.com>
2025-05-29 05:54:13 +0530

    always convert html to jsx (eg. class - classname)

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,46 +1,44 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <NavLink to="/" class="navbar-brand" href="#">
-              Hidden brand
-            </NavLink>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <NavLink to="/" class="nav-link active" aria-current="page" href="#">
-                  Home
-                </NavLink>
-              </li>
-              <li class="nav-item">
-                <NavLink to="/" class="nav-link" href="#">
-                  Link
-                </NavLink>
-              </li>
-              <li class="nav-item">
-                <NavLink to="/" class="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+   <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <navlink to="/" className="navbar-brand" href="#">
+        Hidden brand
+      </navlink>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <navlink to="/" className="nav-link active" aria-current="page" href="#">
+            Home
+          </navlink>
+        </li>
+        <li className="nav-item">
+          <navlink to="/register" className="nav-link" href="#">
+            Register
+          </navlink>
+        </li>
+        <li className="nav-item">
+          <navlink to="/login" className="nav-link" href="#">
+            Login
+          </navlink>
+        </li>
+        <li className="nav-item">
+          <navlink to="/cart" className="nav-link" href="#">
+            Cart (0)
+          </navlink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
     </>
   );
 };
