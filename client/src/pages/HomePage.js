69d50e7fe578d8c2352b33ccec2377bc9e264f08
Rohit <rohitrawat590590@gmail.com>
2025-05-30 08:18:22 +0530

    react-helmet for seo issues

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout/Layout'
 
 const HomePage = () => {
   return (
-    <Layout>
+    <Layout  title={"Home - Ecommerce"}>
         <h1>Home page</h1>
     </Layout>
   )
