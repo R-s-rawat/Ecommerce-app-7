3d440b80cb1912e5ae33715e1838b29c6b27ffe9
Rohit <rohitrawat590590@gmail.com>
2025-05-30 08:23:43 +0530

    use path * instead /* for non existing pages/route

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/policy" element={<Policy />} />
-        <Route path="/*" element={<Pagenotfound />} />
+        <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </>
   );
