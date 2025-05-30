3086582da8039fa442fad1756358dbea4cc1b103
Rohit <rohitrawat590590@gmail.com>
2025-05-30 08:03:30 +0530

    react-helmet= Layout wrapper comp && DEFAULT-PROPS

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
 // // for app.js content to work in b/w this content tags, like <h1> between <layout></layout>, pass {props} to LAYOUT and print {props.children}
 
@@ -15,9 +16,16 @@ import Footer from "./Footer";
 //   )
 // }
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
       {/* anything else you want on the Layout */}
@@ -25,5 +33,12 @@ const Layout = ({ children }) => {
     </div>
   );
 };
+
+Layout.defaultProps = {
+  title:'Ecommerce app',
+  description:'Shop best products online with ease',
+  keywords:'buy,shop,purchase',
+  author: 'rohit'
+}
 
 export default Layout;
