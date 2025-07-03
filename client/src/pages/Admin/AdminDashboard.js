c31c8b7212505a6d55a51d2165f113e4df3c9408
Rohit <rohitrawat590590@gmail.com>
2025-07-03 07:11:04 +0530

    added adminMenu comp. to adminDashboard page..

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Menu/AdminMenu";
 
 const AdminDashboard = () => {
   return (
     <Layout>
-      <h1>Admin Dashboard</h1>
+      {/* <h1>Admin Dashboard</h1> */}
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">Content</div>
+        </div>
+      </div>
     </Layout>
   );
 };
