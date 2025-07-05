70967f184003e3b31c63bd65e992bbd2968c4ad3
Rohit <rohitrawat590590@gmail.com>
2025-07-05 08:32:14 +0530

    admin dashboard -all pages basic design complete

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Menu/AdminMenu";
 
 const CreateCategory = () => {
   return (
-   <Layout>
-    <h1>Create Category</h1>
-   </Layout>
-  )
-}
+    <Layout title={"Dashboard - Create Category"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Create category</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default CreateCategory
+export default CreateCategory;
--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Menu/AdminMenu";
 
 const CreateProduct = () => {
   return (
-    <Layout>
-        <h1>Create Product</h1>
+    <Layout title={"Dashboard - Create Product"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Create product</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default CreateProduct
+export default CreateProduct;
