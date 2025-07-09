5afeeb56517588441a6be96f5b754215a0a55f77
Rohit <rohitrawat590590@gmail.com>
2025-07-09 08:56:33 +0530

    some error in category crud operation(admin panel)

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Menu/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const CreateCategory = () => {
+  //  have an empty array for storing multiple values
+  const [categories, setCategories] = useState([]);
+
+  // get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data.success) {
+        setCategories(data.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting categories");
+    }
+  };
+
+  // for showing fetched categories
+  useEffect(() => {
+    getAllCategory();
+  }, []);
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
@@ -11,7 +33,26 @@ const CreateCategory = () => {
             <AdminMenu />
           </div>
           <div className="col-md-9">
-            <h1>Create category</h1>
+            {/* <h1>Create category</h1> */}
+            <h1>Manage category</h1>
+            <div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                   {categories.map(c =>{
+                    <td key={c._id}>{c.name}</td>
+
+                   })}
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
