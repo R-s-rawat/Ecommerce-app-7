46e4206bf039242640f0e184cf495cb468602b63
Rohit <rohitrawat590590@gmail.com>
2025-07-10 23:09:33 +0530

    manage category(update category) admin panel done

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -4,6 +4,7 @@ import AdminMenu from "../../components/Menu/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
 import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 
 const CreateCategory = () => {
   const API =
@@ -15,6 +16,10 @@ const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
 
   const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+  // states for modal
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
 
   // get all categories
   const getAllCategory = async () => {
@@ -40,10 +45,10 @@ const CreateCategory = () => {
       );
       if (data?.success) {
         // toast.success(`${data.name} is created`);
-         toast.success(`${name} is created`);
-        getAllCategory()
+        toast.success(`${name} is created`);
+        getAllCategory();
       } else {
-        toast.error(data.message)
+        toast.error(data.message);
       }
     } catch (error) {
       console.log(error);
@@ -51,22 +56,54 @@ const CreateCategory = () => {
     }
   };
 
+  // update category on modal's form submit
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      // console.log(e)
+      const { data } = await axios.put(
+        `${API}/api/v1/category/update-category/${selected._id}`,
+        { name: updatedName }
+      );
+      if (data.success) {
+        // toast.success(data.message);
+        toast.success(`${updatedName} is updated`)
+        setSelected(null)
+        setUpdatedName("")
+        setVisible(false)
+        getAllCategory()
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error while updating category");
+    }
+  };
+
   // for showing fetched categories
   useEffect(() => {
     getAllCategory();
   }, []);
+
   return (
     <Layout title={"Dashboard - Create Category"}>
       <div className="container-fluid m-3 p-3">
         <div className="row">
+          {/* 1st column */}
           <div className="col-md-3">
             <AdminMenu />
           </div>
+          {/* 2nd column */}
           <div className="col-md-9">
             {/* <h1>Create category</h1> */}
             <h1>Manage category</h1>
             <div className="p-3 w-50">
-              <CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName} />
+              <CategoryForm
+                handleSubmit={handleSubmit}
+                value={name}
+                setValue={setName}
+              />
             </div>
             <div className="w-75">
               <table className="table">
@@ -82,7 +119,20 @@ const CreateCategory = () => {
                       <tr>
                         <td key={c._id}>{c.name}</td>
                         <td>
-                          <button className="btn btn-primary">Edit</button>
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => setVisible(true)}
+                            onClick={() => {
+                              setVisible(true);
+                              setUpdatedName(c.name);
+                              setSelected(c)
+                            }}
+                          >
+                            Edit
+                          </button>
+                          <button className="btn btn-danger ms-2">
+                            Delete
+                          </button>
                         </td>
                       </tr>
                     </>
@@ -90,6 +140,17 @@ const CreateCategory = () => {
                 </tbody>
               </table>
             </div>
+            <Modal
+              onCancel={() => setVisible(false)}
+              footer={null}
+              open={visible}
+            >
+              <CategoryForm
+                value={updatedName}
+                setValue={setUpdatedName}
+                handleSubmit={handleUpdate}
+              />
+            </Modal>
           </div>
         </div>
       </div>
