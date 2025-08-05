4f08846e9ac103e3c04f096fd7a15d8914b9effe
Rohit <rohitrawat590590@gmail.com>
2025-08-05 06:37:03 +0530

    getting orders data in- users orders page

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -1,8 +1,35 @@
 // import React from "react";
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import UserMenu from "../../components/Menu/UserMenu";
+import axios from "axios";
+import { useAuth } from "../../context/auth";
 
 const Orders = () => {
+  const API =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_API
+      : "http://localhost:8080";
+
+  const [orders, setOrders] = useState([]);
+
+  const [auth,setAuth] = useAuth();
+
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get(`${API}/api/v1/auth/orders`);
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+if(auth?.token){
+  getOrders(); 
+}
+  },[auth?.token])
+
   return (
     <Layout title={"Your orders"}>
       <div className="container-fluid p-3 m-3">
@@ -12,6 +39,7 @@ const Orders = () => {
           </div>
           <div className="col-md-9">
             <h1>All orders</h1>
+            <p>{JSON.stringify(orders, null, 4)}</p>
           </div>
         </div>
       </div>
