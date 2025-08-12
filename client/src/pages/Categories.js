aab4533907994264cd91f8b3646066565ed877e6
Rohit <rohitrawat590590@gmail.com>
2025-08-12 08:43:21 +0530

    categories checks

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -12,11 +12,13 @@ const Categories = () => {
 
   const categories = useCategory();
 
+  console.log(categories)
+
   return (
     <Layout title={"All Categories"}>
       <div className="container">
         <div className="row">
-          {categories.map((c) => (
+          {Array.isArray(categories) && categories.map((c) => (
             <div className="col-md-6 mt-5 mb-3 gx-3 gy-3" key={c._id}>
               <Link to={`/category/${c.slug}`} className="btn btn-primary">
                 {c.name}
