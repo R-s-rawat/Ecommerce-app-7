6ffb7493b937b4800f9ad5426aeb718f3653c64e
Rohit <rohitrawat590590@gmail.com>
2025-08-15 10:21:24 +0530

    improvised css for smaller screens-header

--- client/src/components/Form/SearchbarInputForm.js
+++ client/src/components/Form/SearchbarInputForm.js
@@ -25,23 +25,26 @@ const SearchbarInputForm = () => {
       console.log(error);
     }
   };
+
   return (
-    <div>
-      <form className="d-flex" role="search" onSubmit={handleSubmit}>
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-          value={values.keyword}
-          onChange={(e) => setValues({ ...values, keyword: e.target.value })}
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form>
-    </div>
-  );
+  <div className="searchbar-container">
+    <form className="d-flex" role="search" onSubmit={handleSubmit}>
+      <input
+        className="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        value={values.keyword}
+        onChange={(e) => setValues({ ...values, keyword: e.target.value })}
+      />
+      <button className="btn btn-outline-light" type="submit">
+        Search
+      </button>
+    </form>
+  </div>
+);
+
+
 };
 
 export default SearchbarInputForm;
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -7,6 +7,7 @@ import useCategory from "../../hooks/useCategory";
 import { useCart } from "../../context/cart";
 import { Badge } from "antd";
 import siteLogo from '../../images/ecommerceLogo.jpg'
+import '../../styles/HeaderStyles.css'
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
@@ -30,186 +31,102 @@ const Header = () => {
     toast.success("Logout successfully");
   };
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          {/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link to="/" className="navbar-brand">
-             <img src={siteLogo} alt="Site logo image" width="25%" />
-            </Link>
-            {/* <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </a>
-              {categories?.map((c) => (
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      {c.name}
-                    </a>
-                  </li>
-                </ul>
-              ))}
-            </li> */}
+  <header className="header-style" style={{ backgroundColor: 'black' }}>
+  <nav className="navbar navbar-expand-lg">
+    <div className="container-fluid d-flex justify-content-between align-items-center">
 
-            {/* // BS-5 utility classes  (ME)margin-end: auto && (MS)margin-start: auto*/}
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              {" "}
-              {/* nav-bar is a ul,, and shows many li*/}
-              <SearchbarInputForm />
-              {/* // Home - 1st list-item-navbar */}
-              <li className="nav-item">
-                <NavLink to="/" className="nav-link">
-                  Home
+      {/* Left: Toggler and Logo */}
+      <div className="d-flex align-items-center">
+        {/* Hamburger Menu Icon */}
+        <button
+          className="navbar-toggler me-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        {/* Logo */}
+        <Link to="/" className="navbar-brand m-0 d-none d-sm-block">
+          <img src={siteLogo} alt="Site logo" width="80"  />
+        </Link>
+      </div>
+
+      {/* Right: Searchbar (visible on all screen sizes) */}
+      <div className="d-flex align-items-center">
+        <SearchbarInputForm />
+      </div>
+    </div>
+
+    {/* Collapsible Content */}
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+        {/* You can remove this Searchbar if it's now above */}
+        {/* <li className="nav-item d-none d-lg-block">
+          <SearchbarInputForm />
+        </li> */}
+
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+        </li>
+
+        <li className="nav-item dropdown">
+          <Link className="nav-link dropdown-toggle" to="/categories" data-bs-toggle="dropdown">
+            Categories
+          </Link>
+          <ul className="dropdown-menu">
+            <li>
+              <Link className="dropdown-item" to="/categories">All Categories</Link>
+            </li>
+            {categories?.map((c) => (
+              <li key={c._id}>
+                <Link className="dropdown-item" to={`/category/${c.slug}`}>{c.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </li>
+
+        {!auth.user ? (
+          <>
+            <li className="nav-item">
+              <NavLink to="/register" className="nav-link">Register</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/login" className="nav-link">Login</NavLink>
+            </li>
+          </>
+        ) : (
+          <li className="nav-item dropdown">
+            <NavLink className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown">
+              {auth?.user?.name}
+            </NavLink>
+            <ul className="dropdown-menu">
+              <li>
+                <NavLink className="dropdown-item" to={`/dashboard/${auth?.user?.role === 1 ? 'admin' : 'user'}`}>
+                  Dashboard
                 </NavLink>
               </li>
-              {/* // Home Ends */}
-              {/*------------------------------------------------------- */}
-              {/* // Category - 2nd list-item-navbar */}
-             
-                <li className="nav-item dropdown">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to={"/categories"}
-                  data-bs-toggle="dropdown"
-                >
-                  Categories
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to={"/categories"}>
-                      All Categories
-                    </Link>
-                  </li>
-                  {categories?.map((c) => (
-                    <li>
-                      <Link
-                        className="dropdown-item"
-                        to={`/category/${c.slug}`}
-                      >
-                        {c.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+              <li>
+                <NavLink className="dropdown-item" onClick={handleLogout} to="/login">Logout</NavLink>
               </li>
-              {/* //Category Ends */}
-              {/*------------------------------------------------------- */}
-              {
-                // !auth.user ? (false) : (true)
-              }
-              {!auth.user ? (
-                <>
-                  {/* //Register -  list-item-navbar(auth based) */}
-                  <li className="nav-item">
-                    <NavLink to="/register" className="nav-link">
-                      Register
-                    </NavLink>
-                  </li>
-                  {/* //Register Ends */}
-                  {/*------------------------------------------------------- */}
-                  {/* //Login -  list-item-navbar(auth based)*/}
-                  <li className="nav-item">
-                    <NavLink to="/login" className="nav-link">
-                      Login
-                    </NavLink>
-                  </li>
-                  {/* // Login Ends */}
-                  {/*------------------------------------------------------- */}
-                </>
-              ) : (
-                <>
-                  {/* // role based authentication begins -  list-item-navbar(auth based) */}
-                  <li className="nav-item dropdown">
-                    <NavLink
-                      className="nav-link dropdown-toggle"
-                      href="#"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      {auth?.user?.name}
-                    </NavLink>
-                    <ul className="dropdown-menu">
-                      {/* for dashboard */}
-                      <li>
-                        <NavLink
-                          className="dropdown-item"
-                          to={`/dashboard/${
-                            auth?.user?.role === 1 ? "admin" : "user"
-                          }`}
-                        >
-                          Dashboard
-                        </NavLink>
-                      </li>
-                      {/* for logout */}
-                      <li>
-                        <NavLink
-                          onClick={handleLogout}
-                          to="/login"
-                          // className="nav-link"
-                          className="dropdown-item"
-                        >
-                          Logout
-                        </NavLink>
-                      </li>
-                      {/* <li>
-                        <hr className="dropdown-divider" />
-                      </li> */}
-                    </ul>
-                  </li>
-                  {/* // role based authentication ends */}
-                  {/* //logout */}
-                  {/* <li className="nav-item">
-                    <NavLink
-                      onClick={handleLogout}
-                      to="/login"
-                      className="nav-link"
-                    >
-                      Logout
-                    </NavLink>
-                  </li> */}
-                  {/* //logout Ends */}
-                  {/*------------------------------------------------------- */}
-                </>
-              )}
-              {/* // Cart -  list-item-navbar */} 
-              <li className="nav-item">
-                {/* <NavLink to="/cart" className="nav-link">
-                  Cart {cart?.length}
-                </NavLink> */}
-                <Badge count={cart?.length} showZero>
-                  <NavLink to='/cart' className='nav-link'>
-                  {/* Cart {cart?.length} */}
-                  Cart
-                  </NavLink>
-                </Badge>
-              </li>
-              {/* // Cart Ends */}
             </ul>
-          </div>
-          {/* /////////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
-        </div>
-      </nav>
-    </>
+          </li>
+        )}
+
+        <li className="nav-item">
+          <Badge count={cart?.length} showZero>
+            <NavLink to="/cart" className="nav-link">Cart</NavLink>
+          </Badge>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</header>
+
   );
 };
 
